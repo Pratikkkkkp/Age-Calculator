a06ed7f8f3152928ea177c81bee2a6861c63fbb3
Pratik Pal <palpratik420@gmail.com>
2025-01-05 22:45:04 +0530

    months added

--- index.js
+++ index.js
@@ -7,25 +7,32 @@ function calculateAge() {
   if (birthdayValue === "") {
     alert("Please enter your birthday");
   } else {
-    const age = getAge(birthdayValue);
-    resultEl.innerText = `Your age is ${age} ${age > 1 ? "years" : "year"} old`;
+    const { years, months } = getAge(birthdayValue);
+    resultEl.innerText = `Your age is ${years} ${
+      years > 1 ? "years" : "year"
+    } and ${months} ${months > 1 ? "months" : "month"} old`;
   }
 }
 
 function getAge(birthdayValue) {
   const currentDate = new Date();
   const birthdayDate = new Date(birthdayValue);
-  let age = currentDate.getFullYear() - birthdayDate.getFullYear();
-  const month = currentDate.getMonth() - birthdayDate.getMonth();
 
-  if (
-    month < 0 ||
-    (month === 0 && currentDate.getDate() < birthdayDate.getDate())
-  ) {
-    age--;
+  let years = currentDate.getFullYear() - birthdayDate.getFullYear();
+  let months = currentDate.getMonth() - birthdayDate.getMonth();
+
+  // Adjust for days of the month
+  if (currentDate.getDate() < birthdayDate.getDate()) {
+    months--;
   }
 
-  return age;
+  // Handle negative months
+  if (months < 0) {
+    years--;
+    months += 12;
+  }
+
+  return { years, months };
 }
 
 btnEl.addEventListener("click", calculateAge);
